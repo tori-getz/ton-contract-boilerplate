89286ffc576ddf27e0737bd0d9e4fe14524eedf6
getzzZ <crotecnhics@gmail.com>
2022-11-22 06:07:04 +0300

    initial commit

---
+++ scripts/build.ts
@@ -0,0 +1,63 @@
+import { logger } from "./util";
+import * as fs from 'fs-extra';
+import * as path from 'node:path';
+import child_process from 'node:child_process';
+import * as appRoot from 'app-root-path';
+import glob from 'fast-glob';
+import { config } from 'dotenv';
+import { Cell } from 'ton';
+
+const BUILD_FOLDER = path.resolve(appRoot.path, 'build');
+const ARTIFACTS_FOLDER = path.resolve(appRoot.path, 'build', 'artifacts');
+const CONTRACTS_PATH = path.resolve(appRoot.path, 'contracts', '*.fc');
+const ENV_PATH = path.resolve(appRoot.path, '.env');
+
+config({ path: ENV_PATH });
+
+const build = async () => {
+    logger.info('* Build contracts');
+
+    logger.info('* Erase /build folder')
+    await fs.remove(BUILD_FOLDER);
+    await fs.ensureDir(BUILD_FOLDER);
+    await fs.ensureDir(ARTIFACTS_FOLDER);
+
+    logger.info('* Find root contracts');
+
+    const rootContracts = await glob.sync(CONTRACTS_PATH)
+    for (const rootContract of rootContracts) {
+        logger.info(`  Found root contract: ${rootContract}... building`);
+
+        const contractName = path.parse(rootContract).name;
+        const contractPath = path.resolve(appRoot.path, 'contracts', rootContract);
+
+        const fifArtifact = path.resolve(ARTIFACTS_FOLDER, `${contractName}.fif`);
+        const fiftCellArtifact = path.resolve(ARTIFACTS_FOLDER, `${contractName}.cell.fif`);
+        const cellArtifact = path.resolve(ARTIFACTS_FOLDER, `${contractName}.cell`);
+        const hexArtifact = path.resolve(BUILD_FOLDER, `${contractName}.compiled.json`);
+
+        await child_process.execSync(`func -APS -o ${fifArtifact} ${contractPath} 2>&1 1>node_modules/.tmpfun`);
+        logger.info(`  + new artifact ${fifArtifact}`);
+
+        let fiftCellSource = [
+            // '"Asm.fif" include',
+            await fs.readFileSync(fifArtifact).toString(),
+            `boc>B "${cellArtifact}" B>file`
+        ].join('\n');
+        await fs.ensureFile(fiftCellArtifact);
+        await fs.writeFileSync(fiftCellArtifact, fiftCellSource);
+        logger.info(`  + new artifact ${fiftCellArtifact}`);
+
+        console.log(process.env.FIFTPATH)
+
+        await child_process.execSync(`fift -I ${process.env.FIFTPATH} ${fiftCellArtifact}`);
+
+        const hex = Cell.fromBoc(fs.readFileSync(cellArtifact))[0].toBoc().toString('hex');
+        fs.writeJSONSync(hexArtifact, { hex });
+        logger.info(`  + new artifact ${hexArtifact}`);
+
+        logger.info(`  ${contractName}.compiled.json created!`);
+    }
+}
+
+build();
---
+++ scripts/create-deploy-config.ts
@@ -0,0 +1,27 @@
+import { mnemonicNew } from "ton-crypto";
+import { logger } from './util';
+import * as fs from 'fs-extra';
+import * as path from 'node:path';
+import * as appRoot from 'app-root-path';
+
+const CONFIG_FILENAME = 'deploy.config.json';
+
+const createDeployConfig = async () => {
+    logger.info(`Create ${CONFIG_FILENAME}`);
+
+    const configPath = path.resolve(appRoot.path, CONFIG_FILENAME);
+
+    const deployerWallet = 'org.ton.wallets.v3.r2';
+    const deployerMnemonic = (await mnemonicNew(24)).join(' ');
+
+    logger.info(`Wallet type: ${deployerWallet}`);
+    logger.info(`Mnemonic: ${deployerMnemonic}`);
+
+    const config = { deployerWallet, deployerMnemonic };
+
+    await fs.writeJSON(configPath, config, { spaces: 4 });
+
+    logger.info(`${configPath} created!`)
+}
+
+createDeployConfig();
